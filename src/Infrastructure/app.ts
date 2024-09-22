b8264114bc85a60a3d007fc35a9188f2952262c5
ToanNguyenThai <19522366@gm.uit.edu.vn>
2024-09-22 20:42:07 +0700

    chore: migration

--- src/Infrastructure/app.ts
+++ src/Infrastructure/app.ts
@@ -4,19 +4,29 @@ import { config } from "./config";
 import Server from "@Infrastructure/server/express";
 import { errorMiddleware } from "@Presentation/middlewares/error";
 import { configureRoutes } from "@Presentation/routes";
+import { AppDataSource } from "./database/typeorm";
 
 const app = express();
 
-new Server(app, configureRoutes, errorMiddleware);
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected successfully!");
 
-app
-  .listen(config.PORT as number, function () {
-    console.info(`Local is running on http://localhost:8000`);
+    // Start the server once the database is connected
+    new Server(app, configureRoutes, errorMiddleware);
+
+    app
+      .listen(config.PORT as number, () => {
+        console.info(`Server is running on http://localhost:${config.PORT}`);
+      })
+      .on("error", (err: any) => {
+        if (err.code === "EADDRINUSE") {
+          console.log("Server startup error: address already in use");
+        } else {
+          console.log(err);
+        }
+      });
   })
-  .on("error", (err: any) => {
-    if (err.code === "EADDRINUSE") {
-      console.log("Server startup error: address already in use");
-    } else {
-      console.log(err);
-    }
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
   });
--- src/Infrastructure/database/typeorm/index.ts
+++ src/Infrastructure/database/typeorm/index.ts
@@ -1,16 +1,21 @@
 import { config } from "@Infrastructure/config";
 import { DataSource } from "typeorm";
+import path from "path";
+import { User } from "@Domain/entities/User";
+import { UserPersonalInfo } from "@Domain/entities/UserPersonalInfo";
+import { Task } from "@Domain/entities/Task";
+import { SubTask } from "@Domain/entities/SubTask";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: config.DB_HOST,
     port: config.DB_PORT as number,
     username: config.DB_USERNAME,
-    password: config.DB_PASSWORD,
+    password: undefined,
     database: config.DB_SCHEMA,
     synchronize: true,
     logging: true,
-    entities: [],
+    entities: [User, UserPersonalInfo, Task, SubTask],
     subscribers: [],
-    migrations: [],
+    migrations: [path.join(__dirname, "./migrations/*.ts")],
 })
